e52c6a38dfb560f73ca9b8057e636dbeecea2a9f
Yuriy Yunusov <yunusovyf@gmail.com>
2024-05-26 13:42:20 +0500

    add logger

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,9 +1,11 @@
 from sqlalchemy import select, and_, or_, func
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.bookings.dto import SBookingCreateDTO
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
+from app.logger import logger
 
 
 class BookingDAO(BaseDAO[Bookings]):
@@ -11,26 +13,38 @@ class BookingDAO(BaseDAO[Bookings]):
 
     @classmethod
     async def get_qty_booked_room(cls, booking_data: SBookingCreateDTO) -> int:
-        async with async_session_maker() as session:
-            get_qty_booked_rooms = (
-                select(func.count(cls.model.id))
-                .where(
-                    and_(
-                        cls.model.room_id == booking_data.room_id,
-                        or_(
-                            and_(
-                                cls.model.date_from >= booking_data.date_from,
-                                cls.model.date_from <= booking_data.date_to,
+        try:
+            async with async_session_maker() as session:
+                get_qty_booked_rooms = (
+                    select(func.count(cls.model.id))
+                    .where(
+                        and_(
+                            cls.model.room_id == booking_data.room_id,
+                            or_(
+                                and_(
+                                    cls.model.date_from >= booking_data.date_from,
+                                    cls.model.date_from <= booking_data.date_to,
+                                ),
+                                and_(
+                                    cls.model.date_from <= booking_data.date_from,
+                                    cls.model.date_to > booking_data.date_from,
+                                ),
                             ),
-                            and_(
-                                cls.model.date_from <= booking_data.date_from,
-                                cls.model.date_to > booking_data.date_from,
-                            ),
-                        ),
+                        )
                     )
                 )
-            )
-            qty_booked_rooms = await session.execute(get_qty_booked_rooms)
-            qty_booked_rooms: int = qty_booked_rooms.scalar()
+                get_qty_booked_rooms = await session.execute(get_qty_booked_rooms)
+                qty_booked_rooms: int = get_qty_booked_rooms.scalar()
 
-            return qty_booked_rooms
+                return qty_booked_rooms
+        except (SQLAlchemyError, Exception) as e:
+            msg = 'Unknown'
+            if isinstance(e, SQLAlchemyError):
+                msg = 'Database'
+            msg += ' Exc: Cannot get qty booked rooms'
+
+            logger.error(
+                msg,
+                extra=booking_data.dict(),
+                exc_info=True
+            )
